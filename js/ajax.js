35a33e8a387d032fb23ff758be20debdc99074ba
Nani0002 <molnardani002@gmail.com>
2025-04-24 19:54:46 +0200

    init3

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,21 @@
 window.addEventListener("load", init, false);
 
-function init(){
-	console.log("AAA");
+function init() {
+	$.ajax({
+		url: "https://baconipsum.com/api/?type=all-meat",
+		type: "GET",
+		success: function (response) {
+			alert(response);			
+		},
+		error: function (xhr) {
+			let response = JSON.parse(xhr.responseText);
+			if (xhr.status === 422) {
+				handleAjaxErrors(xhr.responseJSON.errors);
+			} else if (xhr.status === 401 && response.redirect) {
+				window.location.href = response.redirect;
+			} else {
+				alert("An error occurred: " + response.error);
+			}
+		},
+	});
 }
